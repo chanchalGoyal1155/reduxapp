3526c239bb4d1bc645e0661af75e7474d4c79e0f
chanchal goyal <miss71910@gmail.com>
2024-09-10 23:14:43 +0530

    introduce redux

---
+++ src/Shop.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div>
+      <h2>Deposite/Withdraw Money</h2>
+      <button type="button" class="btn btn-primary mx-2">
+        -
+      </button>
+      Update Balance
+      <button type="button" class="btn btn-primary mx-2">
+        +
+      </button>
+    </div>
+  );
+};
+
+export default Shop;
---
+++ src/state/action-creators/index.js
@@ -0,0 +1,17 @@
+export const depositeMoney = (amount) => {
+  return (dispatch) => {
+    dispatch({
+      type: "deposite",
+      payload: amount,
+    });
+  };
+};
+
+export const withdrawMoney = (amount) => {
+  return (dispatch) => {
+    dispatch({
+      type: "withdraw",
+      payload: amount,
+    });
+  };
+};
